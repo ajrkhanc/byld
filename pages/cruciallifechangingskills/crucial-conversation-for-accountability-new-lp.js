fbe56ba51ec6231d663bf12b67296441298f0ff8
Ajr khan <git config --global user.email you@example.com>
2022-12-16 15:41:06 +0530

    dd

--- pages/cruciallifechangingskills/crucial-conversation-for-accountability-new-lp.js
+++ pages/cruciallifechangingskills/crucial-conversation-for-accountability-new-lp.js
@@ -159,6 +159,8 @@ export default function BrowseCourses() {
                                     <select name='slot' required>
                                         <option value="">Pick any Slot*</option>
                                         <option value="Workshop - December 22nd - 23rd, 2022">Workshop - December 22nd - 23rd, 2022</option>
+                                        <option value="Crucial Conversations for Accountability – January 18th, 2023">Crucial Conversations for Accountability – January 18th, 2023</option>
+                                        <option value="Crucial Conversations for Accountability - February 23rd - 24th, 2023">Crucial Conversations for Accountability - February 23rd - 24th, 2023</option>
                                     </select>
                                 </div>
 
@@ -219,7 +221,7 @@ export default function BrowseCourses() {
                             <div class="row workshop-row">
                                 <div class="col-md-12 workshop-content">
                                     <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
-                                    <h4>CRUCIAL <br /> CONVERSATIONS</h4>
+                                    <h4>CRUCIAL CONVERSATIONS</h4>
                                     <p>for Accountability</p>
                                 </div>
                                 <h5 className='eventt'>Workshop</h5>
@@ -230,6 +232,38 @@ export default function BrowseCourses() {
                                 </div>
                             </div>
                         </div>
+
+                        <div class="col-md-4 workshop-col">
+                            <div class="row workshop-row">
+                                <div class="col-md-12 workshop-content">
+                                    <img class="left-image img100px mbb-30" src="/classets/img/logo-conversations.svg" alt="" />
+                                    <h4>CRUCIAL CONVERSATIONS</h4>
+                                    <p>for Accountability</p>
+                                </div>
+                                <h5 className='eventt'>Webinar</h5>
+                                <div class="col-md-12 workshop-date">
+                                    <h6><i class="fa fa-calendar" aria-hidden="true"></i> January 18th, 2023</h6>
+                                    <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 4.00 PM to 5.00 PM</h6>
+                                    <a href='#registered'><button class="register">REGISTER NOW</button></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 workshop-col"> 
+                       <div class="row workshop-row"> 
+                            <div class="col-md-12 workshop-content"> 
+                            <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
+                            <h4>CRUCIAL CONVERSATIONS</h4>
+                            <p>for Accountability</p>
+                            </div>
+                            <h5 className='eventt'>Workshop</h5>
+                             <div class="col-md-12 workshop-date"> 
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> February 23rd - 24th, 2023</h6>
+                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10.00 AM to 4.30 PM</h6>
+                            <a href='#registered'><button class="register">REGISTER NOW</button></a>
+                            </div>
+                       </div>
+                    </div>
                         
 
                     </div>
