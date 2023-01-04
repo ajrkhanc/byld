afee55b10a03bdf218b5ad8f67208873c0c1f36d
Ajr khan <git config --global user.email you@example.com>
2023-01-04 11:55:29 +0530

    ttyy

--- pages/yogesh-sood/[blogsSlug].js
+++ pages/yogesh-sood/[blogsSlug].js
@@ -6,84 +6,81 @@ import moment from 'moment'
 export async function getServerSideProps(context) {
     const slug = context.params.blogsSlug;
     const post5 = await fetch(`https://yogeshsood.onrender.com/api/post/${slug}`)
-    const posts = await post5.json()   
+    const posts = await post5.json()
 
 
 
 
     return {
-        props: {            
-            posts,            
+        props: {
+            posts,
         },
     }
 }
 
 
 
-export default function singleblog({posts }) {
+export default function singleblog({ posts }) {
 
 
     return (
         <>
             <Head>
-            <title>{posts.Title}</title>
-                <meta name="description" content="" />
+                <title>{posts.Title}</title>
+                <meta name="description" content={posts.Description} />
                 <link rel="icon" href="/favicon.png" />
 
                 {/* Twitter */}
-      {/* <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@blanchard_india" />
-      <meta name="twitter:creator" content="@blanchard_india" />
-      <meta name="twitter:title" content={postdetail.title.rendered} />
-      <meta name="twitter:description" content="" />
-      <meta name="twitter:image" content={postdetail.yoast_head_json['og_image']['0'].url} />
-      <meta property="twitter:url" content={postdetail.slug} /> */}
-    
-      {/* Open Graph / Facebook */}
-      {/* <meta property="og:locale" content="en-in" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Blanchard International" />
-      <meta property="og:title" content={postdetail.title.rendered} />
-      <meta property="og:description" content="" />
-      <meta property="og:image" name="image" content={postdetail.yoast_head_json['og_image']['0'].url} />
-      <meta property="og:url" content={postdetail.slug} /> */}
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@YomaSolutions" />
+                <meta name="twitter:creator" content="@YomaSolutions" />
+                <meta name="twitter:title" content={posts.Title} />
+                <meta name="twitter:description" content={posts.Description} />
+                <meta name="twitter:image" content={posts.ImageURL} />
+                <meta property="twitter:url" content={posts.posturl} />
 
-                
-
+                {/* Open Graph / Facebook */}
+                <meta property="og:locale" content="en-in" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="YOMABusinessSolutions" />
+                <meta property="og:title" content={posts.Title} />
+                <meta property="og:description" content={posts.Description} />
+                <meta property="og:image" name="image" content={posts.ImageURL} />
+                <meta property="og:url" content={posts.posturl} />
             </Head>
-            
+
             <div class="rs-inner-blog pt-50 pb-50 md-pt-70 md-pb-70">
                 <div class="container custom">
                     <div class="row">
                         <div className="col-lg-2"></div>
                         <div class="col-lg-8 pr-35 md-pr-15 md-mt-50">
 
-                            
-                        <div class="blog-details">
-                                    <div class="bs-img">
-                                        <a><img src={posts.ImageURL} alt={posts.ImageAlt} /></a>
-                                        <h3 className="wwwcc">{posts.Title}</h3>
-                                    </div>
-                                    <div class="blog-full">
-                                        <ul class="single-post-meta">
+
+                            <div class="blog-details">
+                                <div class="bs-img">
+                                    <a><img src={posts.ImageURL} alt={posts.ImageAlt} /></a>
+                                    <h3 className="wwwcc">{posts.Title}</h3>
+                                </div>
+                                <div class="blog-full">
+                                    <ul class="single-post-meta">
                                         <li>
                                             <span class="p-date"><i class="fa fa-calendar-check-o"></i> {moment(posts.ModifiedDate).format('DD MMMM Y')}</span>
                                         </li>
                                         <li>
                                             <span class="p-date"> <i class="fa fa-user-o"></i> {posts.AuthorFullname}</span>
                                         </li>
-                                        </ul>
-                                        <div className="bloglinkh" dangerouslySetInnerHTML={{ __html: posts.Content }}></div>
-                                  
-                                    </div>
+                                    </ul>
+                                    <div className="bloglinkh" dangerouslySetInnerHTML={{ __html: posts.Content }}></div>
+
                                 </div>
+                            </div>
 
 
 
                         </div>
                     </div>
                 </div>
-                </div>
+            </div>
         </>
     )
 }
