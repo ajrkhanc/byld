1e2f19f95a4ec7c98fb56e92271aad66c92048c4
Ajr khan <git config --global user.email you@example.com>
2023-01-17 15:15:13 +0530

    qw

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   reactStrictMode: true,
-}
 
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
+
+
