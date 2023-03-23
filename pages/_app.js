92e71ec690a0ad6ef03e3708cee6422bd9c46ab8
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-03-23 12:56:00 +0530

    wddasD

--- pages/_app.js
+++ pages/_app.js
@@ -1356,8 +1356,7 @@ gtag('config', 'G-5PXTTR960E');
     return (
       <BYLDNEW>
         <Head>
-          <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" />
-          <link rel="stylesheet" type="text/css" href="/assets/css/homemodule.css" />
+          <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" />          
           
           <link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css" />
 
@@ -1383,6 +1382,7 @@ gtag('config', 'G-5PXTTR960E');
           <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css" />
           <link rel="stylesheet" type="text/css" href="/assets/css/globals.css" />
           <link rel="stylesheet" type="text/css" href="/assets/css/bg.css" />
+          <link rel="stylesheet" type="text/css" href="/assets/css/homemodule.css" />
 
           <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
