ed651db3e965ea15b564dd93625d8018454b8e24
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-03-28 17:45:17 +0530

    eqweqw

--- pages/green.js
+++ pages/green.js
@@ -2,9 +2,45 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import Slider from "react-slick";
-
+import React from 'react';
+import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel, } from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 
 export default function Home() {
+
+    const [modalOpen1, setModalOpen1] = React.useState(false);
+    const whitp1 = async event => {
+        const coursename = 'White Paper';
+        event.preventDefault()
+        document.getElementById("submitbuttonform1").value = "Submitting...."
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function () {
+            console.log(this.responseText);
+        }
+        xhttp.open("Post", 'https://ajrkhan.xyz/cruciallifechangingskills/wp-json/contact-form-7/v1/contact-forms/115/feedback');
+        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4) {
+                if (xhttp.status == 200) {
+                    document.getElementById("showlabel").innerHTML = "Thank you for submitting your details. Our subject matter experts will connect you within 24 working hours.";
+                    document.getElementById("contactForm").reset();
+                    document.getElementById("showlabel").style.display = "block";
+                    window.setTimeout(function () {
+                        window.location.href = "/assets/pdf/challenges-and-recommendations-for-auto-industry.pdf"
+                    });
+
+                } else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        };
+        xhttp.send("name=" + event.target.name.value +
+            "&youremail=" + event.target.email.value +
+            "&coursename=" + coursename)
+    }
+
+
 
     var settings = {
         dots: true,
@@ -51,6 +87,38 @@ export default function Home() {
                 <meta name="description" content="Eagle’s Flight offers corporate team building games in a virtual immersive learning environment for effective self-learning. To know more, check out the page." />
                 <link rel="stylesheet" type="text/css" href="/efassets/css/new.css" />
             </Head>
+
+            <Modal className='toppc mwc500' toggle={() => setModalOpen1(!modalOpen1)} isOpen={modalOpen1} backdrop="static" keyboard={false}>
+                <button aria-label="Close" className="close popcl" type="button" onClick={() => setModalOpen1(!modalOpen1)}>
+                    <span aria-hidden={true}>×</span>
+                </button>
+
+                <ModalBody>
+                    <form id='contactForm' class="row popupfc ccl" onSubmit={whitp1}>
+                        <h4>Any title</h4>
+                        {/* <h5>Please Register to download </h5> */}
+                        <div class="col-md-12">
+                            <input type="text" name="name" class="form-control" placeholder="Enter Name*" required />
+                        </div>
+                        <div class="col-md-12">
+                            <input type="email" name="email" class="form-control" placeholder="Enter Email*" required />
+                        </div>
+                        <div className="col-sm-6 mb-12">
+                            <input type="text" name="phone" class="form-control" maxlength="10" minlength="10" pattern="[0-9]*" placeholder="Phone No.*" required />
+                        </div>
+                        <div className="col-sm-6 mb-12">
+                            <input type="text" name="organization" class="form-control" placeholder="Organization*" required />
+                        </div>
+                        <div className="col-lg-12 mb-12">
+                            <textarea className="from-control" name="leadsquared_Notes" placeholder="Let us know what you are looking for."></textarea>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <input id='submitbuttonform1' class="formbtn" type="submit" value="Submit" />
+                        </div>
+                        <p id="showlabel" style={{ display: "none" }}></p>
+                    </form>
+                </ModalBody>
+            </Modal>
 
             <section className='videosection'>
                 <div class="videowrapper">
@@ -225,12 +293,12 @@ export default function Home() {
                 <div className="container-fluid pa0">
                     <div className="row">
                         <div className="clearfix"></div>
-                        <div className="col-sm-5 brodbg">
+                        <div className="col-sm-6 brodbg">
                             <h2>Passionate About Coaching?</h2>
                             <h3>Enroll for our upcoming 39th Batch!</h3>
                             <h4>STARTING  22ND APRIL <br></br>10:00 AM TO 4:00 PM</h4>
                         </div>
-                        <div className="col-sm-7 brod2bg">
+                        <div className="col-sm-6 brod2bg">
                             <div className="bannerform">
                                 <form id="contact-form" className='clientcornner ptt-40 pbb-20'>
                                     <div className="row">
@@ -322,27 +390,91 @@ export default function Home() {
                     </div>
 
                     <div className="row ptt-40">
-                        <div className='col-sm-12'>
-                            <div class="faq-content">
-                                <div id="accordion" class="accordion">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <a class="card-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true">What is included in the fees of the course?</a>
-                                        </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <a class="card-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true">In Addition to the coaching sessions, what else is included in the program?</a>
-                                        </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <a class="card-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true">Are there easy finance options available?</a>
-                                        </div>
-                                    </div>
+                        <div className='col-sm-12 faqss'>
+                            <Accordion>
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            What is included in the fees of the course?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            The fees of the Coaching Certification includes facilitation fees for Classroom Sessions (Virtual Instructor led) and Asynchronous learning Sessions. We also offer 10 hrs of ‘Mentor Coaching’ sessions, along with ‘Peer Coaching Support’ with unlimited access to Mock Tests for CKA ( now replaced with the new Credentialing Exam). That’s not all, we provide flexible finance options with interest free EMIs. Moreover, various other certifications & programs worth more than Rs. 1 Lakh are complimentary with the course.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            In Addition to the coaching sessions, what else is included in the program?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            In addition, we offer you the following certifications and programs worth more than Rs. 1 lakh, as complimentary :<br></br>
 
-                                </div>
-                            </div>
+                                            ‘Emotional Intelligence Train the Trainer’ certification.<br></br>
+                                            25 free EQ assessments for your coaching assignments.<br></br>
+                                            Free Workshop on setting up your coaching practice or profession.<br></br>
+                                            ‘Advance Learning Professional Certification’ program (a 3 months certification course).<br></br>
+                                            These certifications can be availed anytime within 6 months.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            Are there easy finance options available?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            To reduce your upfront payment burden, we offer ‘Interest Free EMIs’, where you only pay 3 EMIs as advance payment. The rest can be paid over 6/9/12 months depending on the finance institution’s loan tenure scheme.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            Does the institute provide mentor coaching hours or do we have to pay extra?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            At no additional cost, we offer 10 hrs of Mentor Coaching along with ‘Guaranteed Peer Coaching’ assistance and strong back-end support access to Master Coach / Facilitator. This will help you speed up your coaching journey and help establish coaching practice whether inside your organization or as a professional coach.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            How well experienced are the Master Coaches/ Instructors?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            The International Master Coaches who conduct this training are very seasoned coaches with close to 30 years of coaching experience between the two of them. In association with Aster Coaching US (Accredited by ICF), we take pride in sharing with you that we certify and develop high-caliber coaches who meet global standards and are in tune with local, regional and global business practices.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            What is the flexibility of the Coaching Program?
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p>
+                                            Keeping in mind one’s work schedule, we offer ViLT sessions on weekends per your convenience. This helps in finishing classroom credits without work being affected. In case of any unforeseen challenges, you can also opt to discontinue the batch and continue later with the next upcoming batch, at no additional Cost.
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+                            </Accordion>
                         </div>
                     </div>
                 </div>
@@ -351,14 +483,15 @@ export default function Home() {
 
             <div class="ptt-60 pbb-50 lastcc">
                 <div className="container">
-                    <div className="row ">                        
+                    <div className="row ">
                         <div className='col-sm-12 text-center'>
                             <h2>Still have a doubt about coaching ?</h2>
-                            <a href='#'>TALK TO US</a>
+                            <a className='talkbtn' onClick={() => setModalOpen1(!modalOpen1)}>TALK TO US</a>
                         </div>
                     </div>
                 </div>
             </div>
+
 
         </>
     )
